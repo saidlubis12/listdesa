40e9864f4491d2edadca04155afd9211cdf7bc57
ninjaa <alkhudrisaid83@gmail.com>
2020-09-11 07:13:54 +0700

    first commit

---
+++ array1.js
@@ -0,0 +1,8 @@
+var listDesaBAntul = [
+    "argodadi", "argomulyo", "argosuryo", "argosari", 
+    "Bangunjiwo", "banguntapaan", "bantul", "baturetno", "bawuran",
+    "canden", "catuharjo", "cetan srigading", 
+    
+]
+
+console.log(listDesaBAntul)
